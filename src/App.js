21724979277bd2eeee0ec0eccdbfab208f9098b0
Davi Vinicius <davivinicius045@gmail.com>
2023-12-07 22:29:48 -0300

    create styles predefined

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import {Footer, Blog, Features, Header, Possibillity, WhatGPT3 } from './containers';
 import {Brand, CTA, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
